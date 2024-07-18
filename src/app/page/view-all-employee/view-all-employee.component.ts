d8fd0b3b3f3f6c452d47fd0354f7536a94b2d143
Asanka-GAP <asankapradeep0830@gmail.com>
2024-07-18 22:09:34 +0530

    update-employee

--- src/app/page/view-all-employee/view-all-employee.component.ts
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -14,6 +14,15 @@ import { NavComponent } from '../../common/nav/nav.component';
 })
 export class ViewAllEmployeeComponent {
 
+  public employeeObj = {
+    id:undefined,
+    firstName:"",
+    lastName:"",
+    email:"",
+    departmentId:"",
+    roleId:""
+  }
+
   public employeeList:any;
 
   constructor(private http:HttpClient){
@@ -66,6 +75,83 @@ this.http.get("http://localhost:8080/get-all").subscribe(res=>{
           icon: "error"
         });
       }
+    });
+  }
+
+    updateEmployee(employee:any) {
+    Swal.fire({
+      title: "Update Details",
+      html: `
+            <input class="swal2-input" value=${employee.firstName} placeholder="First Name" id="firstName">
+            <input class="swal2-input" value=${employee.lastName} placeholder="Last Name" id="lastName">
+            <input class="swal2-input" value=${employee.email} placeholder="Email" id="email">
+            <select class="swal2-select" aria-label="Default select example" id="department" required>
+      <option selected>Department</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+    <select class="swal2-select" aria-label="Default select example" required id="role">
+      <option selected>Role</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+          `,
+      focusConfirm: false,
+      showCancelButton: true,
+      preConfirm: () => {
+        
+        const firstName=document.getElementById("firstName") as HTMLInputElement;
+        const lastName=document.getElementById("lastName") as HTMLInputElement;
+        const email=document.getElementById("email") as HTMLInputElement;
+        const departmentId=document.getElementById("department") as HTMLSelectElement;
+        const roleId=document.getElementById("role") as HTMLSelectElement;
+
+        this.employeeObj.id=employee.id;
+        this.employeeObj.firstName=firstName.value;
+        this.employeeObj.lastName=lastName.value;
+        this.employeeObj.email=email.value;
+        this.employeeObj.departmentId=departmentId.value;
+        this.employeeObj.roleId=roleId.value;
+        console.log(firstName.value);
+        if (
+          this.employeeObj.firstName == "" ||
+          this.employeeObj.lastName == "" ||
+          this.employeeObj.email == "" ||
+          this.employeeObj.departmentId == "Department"||
+          this.employeeObj.roleId=="Role"
+        ) {
+          Swal.fire({
+            title: "Something Missing!",
+            text: "Opps, something Missing. Please check your registration form",
+            icon: "error",
+            preConfirm: () => {
+              this.updateEmployee(employee);
+            },
+          });
+        } else {
+          this.saveEmployee(this.employeeObj)
+        }
+      },
+    });
+  }
+
+  saveEmployee(employee:any){
+    fetch("http://localhost:8080/update-employee", {
+      method: "PUT",
+      body: JSON.stringify(employee),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    Swal.fire({
+      title: "Updated!",
+      text: "Employee Details Updated!",
+      icon: "success",
+      preConfirm: () => {
+        window.location.reload();
+      },
     });
   }
 }
